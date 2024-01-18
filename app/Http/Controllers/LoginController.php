d3df986d9660342a463d19f07ada1c8253e1ccf7
andersondinizdev <andersondiniz159@gmail.com>
2024-01-18 16:23:25 -0300

    Finalização da primeira api de cadastro e login.

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -18,24 +19,29 @@ class LoginController extends Controller
                 $password = $data['user_password'];
                 $check = 0;
 
+                // Busca o usuário no banco de dados
                 $check_user_email = DB::table('users')->where('email', $email)->first();
 
                 if ($check_user_email) {
+                    // Verifica a senha usando Hash::check
                     if(Hash::check($password, $check_user_email->password)) {
 
+                        // Autenticação bem-sucedida
                         $response['login'] = true;
                         $response['message'] = 'Autenticação bem-sucedida';
                     } else {
+                         // Senha incorreta
                         $response['login'] = false;
                         $response['message'] = 'Senha incorreta';
                     }
                 } else {
+                     // Usuário não encontrado
                     $response['login'] = false;
                     $response['message'] = 'Usuário não encontrado';
                 }
             } catch (\Exception $e) {
-                $response['login'] = false;
-                $response['message'] = "Erro durante a autenticação $e";
+                $response['error'] = true;
+                $response['message'] = $e->getMessage();
             }
         }
         return response()->json($response);
