3e3ce6ba9ccad8cf6051eb67c2dfaebb048c7052
andersondinizdev <andersondiniz159@gmail.com>
2024-01-18 15:36:17 -0300

    Correção na migrate.

--- database/migrations/2024_01_18_072314_create_tokens_table.php
+++ database/migrations/2024_01_18_072314_create_tokens_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('tokens', function (Blueprint $table) {
             $table->id();
-            $table->string('email');
+            $table->string('email')->unique();
             $table->string('token');
             $table->timestamps();
         });
